4a9d738a363314f560c97ffed314f5044fb9e0dc
Taras Rodynenko <t.rodynenko@gmail.com>
2018-05-23 02:15:52 +0300

    add SearchForm template and fix component routing

--- src/containers/AppContainer/AppContainer.js
+++ src/containers/AppContainer/AppContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from 'store/createStore';
-import CoreLayout from '../../layouts/CoreLayout';
+import CoreLayout from 'layouts/CoreLayout';
 
 const AppContainer = () => (
 	<Provider store={store}>
--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import SearchForm from 'components/SearchForm';
 import ResultsList from 'components/ResultsList';
 import './CoreLayout.scss';
 
 const CoreLayout = () => (
 	<div className='main'>
+		<h1>Giphy search</h1>
+		<SearchForm />
 		<ResultsList />
 	</div>
 );
